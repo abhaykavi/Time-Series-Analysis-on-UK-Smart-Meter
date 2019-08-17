803d636fb0a7f6b02a61e3d9b2930e9ac7850ebc
abhaykavi <40325132+abhaykavi@users.noreply.github.com>
2019-08-17 17:54:08 +0530

    Create Time-Series.py

---
+++ Time-Series.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 17 17:38:48 2019
+
+@author: ganes
+"""
+
+import warnings
+#import itertools
+#import numpy as np
+import matplotlib.pyplot as plt
+warnings.filterwarnings("ignore")
+plt.style.use('fivethirtyeight')
+import pandas as pd
+#import statsmodels.api as sm
+import matplotlib
+matplotlib.rcParams['axes.labelsize'] = 14
+matplotlib.rcParams['xtick.labelsize'] = 12
+matplotlib.rcParams['ytick.labelsize'] = 12
+matplotlib.rcParams['text.color'] = 'k'
+
+df = pd.read_csv("Power-Networks-LCL.csv")
+data_top = df.head(5)
+print(data_top)
